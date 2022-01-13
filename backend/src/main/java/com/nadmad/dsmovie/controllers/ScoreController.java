615eb16db8684920047c594639f25742a770dd6b
Nadyne Madureira <madureira.nadyne@gmail.com>
2022-01-13 16:43:37 -0300

    Pontuação salva

---
+++ backend/src/main/java/com/nadmad/dsmovie/controllers/ScoreController.java
@@ -0,0 +1,25 @@
+package com.nadmad.dsmovie.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nadmad.dsmovie.dto.MovieDTO;
+import com.nadmad.dsmovie.dto.ScoreDTO;
+import com.nadmad.dsmovie.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService service;
+	
+	@PutMapping
+	public MovieDTO saveScore(@RequestBody ScoreDTO dto){
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
+	}
+}
---
+++ backend/src/main/java/com/nadmad/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nadmad.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nadmad.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}

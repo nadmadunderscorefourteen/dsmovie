74eea1d3a0bba156713a6cdcc94d6f6d5d0d773b
Nadyne Madureira <madureira.nadyne@gmail.com>
2022-01-14 17:01:15 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,23 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
     
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
-    
+    }, []);
+   
     
     return (
         <>
